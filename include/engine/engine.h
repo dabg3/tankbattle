94401202cbcfed89a728b71369566a8e47e0e3da
Davide Bigotti <bigotti.davide@hotmail.com>
2024-05-21 23:22:09 +0200

    layout ideas

--- include/engine/engine.h
+++ include/engine/engine.h
@@ -1,13 +1,42 @@
 #include <SDL2/SDL.h>
 
+// 0 degrees rotation means the object is oriented right as trigonometry standard
+// clockwise rotation is used:
+// https://en.wikipedia.org/wiki/Rotation_matrix#Non-standard_orientation_of_the_coordinate_system
+// TODO: separate game_object (representing a static object) from movible_object (extension of game_object)
+//  - add another rotation field in movible_object that conforms to what's written above
+//  - the rotation in game_object can be used to render the object in the right direction
+//      i.e if the bitmap image is oriented south, a -90 degree rotation can 
+//          be set to match initial 0 degree movible_object rotation.
+//          Equally, let the image face south but then set movible_object rotation to 90
+
+// struct inheritance 
+// http://www.deleveld.dds.nl/inherit.htm
+
 struct game_object {
+        //? int lifepoints; -1 means indestructible
         SDL_Texture *texture;
         SDL_Rect pos; //bounding box
-        int rotation;
+        int rotation; 
         unsigned int v_size;
         SDL_FPoint vertices[];
 };
 
+struct movible_object {
+        int rotation;
+        struct game_object base;
+};
+
+// awful stuff
+struct composed_object {
+        unsigned int size;
+        union {
+                struct game_object base;
+                struct movible_object movible;
+        } objects[];
+};
+
+// TODO: if vertices array is empty, initialize vertices according to 'dest' rect
 struct game_object * load_obj(SDL_Renderer *renderer, 
                               char bmp_texture_path[], 
                               SDL_Rect dest,
@@ -27,5 +56,17 @@ struct game_object * load_obj(SDL_Renderer *renderer,
         memcpy(obj->vertices, vertices, sizeof(SDL_FPoint) * v_size);
         return obj;
 }
+
+enum rotation {
+        CLOCKWISE = 1,
+        COUNTERCLOCKWISE = -1
+};
+
+enum direction {
+        // TODO: remove N_ (new), fix clash with enum movement
+        N_FORWARD = 1,
+        N_BACKWARD = -1
+};
+
 
 
